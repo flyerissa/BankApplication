54af9ed3646bd0d087edbd4f41fca44e19b7f45f
Aili <flyerissa@gmail.com>
2014-03-11 16:36:13 +0200

    13exercise

---
+++ src/com/luxoft/bankapp/html/resources/js/authorization.js
@@ -0,0 +1,23 @@
+/**
+ * Created by User on 11.03.14.
+ */
+function checkName() {
+    var name = $("#name").val();
+    var checkResult = true;
+    if (name.length < 2) {
+        $("#nameError").html("Please recheck name! It should contain 2 and more letters");
+        checkResult = false;
+    }
+    var firstPasswd = $("#pass").val();
+    if (firstPasswd.length < 5) {
+        $("#passError").html("Too small passwd!");
+        checkResult = false;
+    }
+
+    if (checkResult) {
+        alert("sending..");
+        return true;
+    } else {
+        return false;
+    }
+}
